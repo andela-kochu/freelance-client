42007f0d8eb86d02f2efb7a04ee8262b298c4bf8
andela-kochu <kingsley.ochu@andela.co>
2015-05-14 12:44:43 +0100

    Added signin page Added the social icons  and buttons Created the base framework for signin and signup controllers

--- app/controllers/signup.controller.js
+++ app/controllers/signup.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('freelanceApp')
-  .controller('homeCtrl', ['$scope','sideNav', function($scope, sideNav) {
+  .controller('signupCtrl', ['$scope','sideNav', function($scope, sideNav) {
       $scope.close = sideNav.close;
       $scope.toggle = sideNav.buildToggler;
     }]);
