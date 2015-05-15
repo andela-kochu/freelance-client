4feef977c831f5f995eef2f5d47f4b278ab669f8
andela-kochu <kingsley.ochu@andela.co>
2015-05-15 17:57:04 +0100

    added a conditional to the sentencase case filter

--- app/filters/truncate.filter.js
+++ app/filters/truncate.filter.js
@@ -20,6 +20,9 @@ angular.module('freelanceApp')
   })
   .filter('sentenceCase', function () {
     return function (value) {
+      if(!value) {
+        return;
+      }
       return value.replace(/\w\S*/g, function(txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
