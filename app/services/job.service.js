c33e8f3c7a7b3a106412162ef54c7f4fdaf74518
andela-kochu <kingsley.ochu@andela.co>
2015-05-15 18:04:55 +0100

    changed service name

--- app/services/job.service.js
+++ app/services/job.service.js
@@ -5,35 +5,41 @@ angular.module('freelanceApp').factory('JobService', [function() {
         data: [
                 {
                   title: 'Facebook ads / Lead Generation',
+                  _id: 1,
                   skills: 'Partner Relationship Payroll & Compensation Finance Financial Research Help Desk Free lance journalist Email Administrator',
                   tools: 'WEBOBJECTS YAFL WISE YAML  VRAY',
                   description: 'We havea job opening for the position of Payable Clerk at (Andy Wood Gallery ). Would you like to work from your home and be paid weekly? (Andy Wood Gallery ) specialize in serving the entire countries and a growing export market,  particularly in the supplies of selected Paints including Paints and Arts deco ratings.'
                 },
                 {
+                  _id: 2,
                   title: 'Need website built',
                   skills: 'Web Design E-Commerce / Commercial Websites Internet / Intranet websites Web Development',
                   tools: 'PHP 5.X HTML HTML5 ADOBE FLASH JAVA JAVASCRIPT PHP 4.X',
                   description: 'This should include a good-looking modern theme, versatility, different Flash/Java applets, and a comfortable webstore complete with integration of any required APIs, etc.'
                 },
                 {
+                  _id: 3,
                   title: 'Website extension',
                   skills: 'Webmaster Web Project Management E-Commerce / Commercial Websites Web Development CMS Implementation Web Security Software Development',
                   tools: 'HTML5 PAYPAL API ECOMMERCE JAVASCRIPT CVS CMS (CONTENT MANAGEMENT SYSTEM)',
                   description: '1. Analytics for current kit builder (how many visitors, how long they spend, how far they get in the process, where do they go) 2. Add products to existing website 3. Update admin CMS to extend data scheme and management'
                 },
                 {
+                  _id: 4,
                   title: 'Facebook ads / Lead Generation',
                   skills: 'Partner Relationship Payroll & Compensation Finance Financial Research Help Desk Free lance journalist Email Administrator',
                   tools: 'WEBOBJECTS YAFL WISE YAML  VRAY',
                   description: 'We havea job opening for the position of Payable Clerk at (Andy Wood Gallery ).  Would you like to work from your home and be paid weekly? (Andy Wood Gallery ) specialize in serving the entire countries and a growing export market, particularly in the supplies of selected Paints including Paints and Arts deco ratings.'
                 },
                 {
+                  _id: 5,
                   title: 'Need website built',
                   skills: 'Web Design E-Commerce / Commercial Websites Internet / Intranet websites Web Development',
                   tools: 'PHP 5.X HTML HTML5 ADOBE FLASH JAVA JAVASCRIPT PHP 4.X',
                   description: 'This should include a good-looking modern theme, versatility, different Flash/Java applets, and a comfortable webstore complete with integration of any required APIs, etc.'
                 },
                 {
+                  _id: 6,
                   title: 'Website extension',
                   skills: 'Webmaster Web Project Management E-Commerce / Commercial Websites Web Development CMS Implementation Web Security Software Development',
                   tools: 'HTML5 PAYPAL API ECOMMERCE JAVASCRIPT CVS CMS (CONTENT MANAGEMENT SYSTEM)',
