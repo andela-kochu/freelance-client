9bdaa45e882d2e761f336729031b7b1e9f35a388
andela-kochu <kingsley.ochu@andela.co>
2015-05-17 21:28:15 +0100

    Added the edit user modal and the delete user confirm box

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('freelanceApp')
-  .controller('profileCtrl', ['$scope', '$location', '$window',
-            'UserService', 'AuthService', 'profilePromise', '$mdDialog',
-    function ($scope, $location, $window, UserService, AuthService, profilePromise, $mdDialog) {
+  .controller('profileCtrl', ['$scope', '$rootScope', '$location', '$window',
+            'UserService', 'AuthService', '$mdDialog', '$timeout',
+    function ($scope, $rootScope, $location, $window, UserService, AuthService, $mdDialog, $timeout) {
 
-      $scope.profile = profilePromise;
+      $scope.profile = UserService.userData;
 
       $scope.editUser = function(ev) {
         $mdDialog.show({
@@ -20,14 +20,63 @@ angular.module('freelanceApp')
           });
       };
       function editUserCtrl($scope, $mdDialog) {
-        $scope.hide = function() {
-          $mdDialog.hide();
-        };
         $scope.cancel = function() {
           $mdDialog.cancel();
         };
-        $scope.answer = function(answer) {
-          $mdDialog.hide(answer);
+         $scope.name = UserService.userData.name;
+         $scope.phone = UserService.userData.phoneNumber;
+         $scope.interests = UserService.userData.interests;
+         $scope.skills = UserService.userData.skill;
+         $scope.gender = UserService.userData.gender;
+    //    $scope.profile = UserService.userData;
+        $scope.hideProg = true;
+        $scope.editProf = function() {
+          $scope.hideProg = false;
+          var formData = {
+                  name: $scope.name,
+                  password: $scope.password,
+                  phoneNumber: $scope.phone,
+                  interests: $scope.interests,
+                  skill: $scope.skills,
+                  gender: $scope.gender
+              };
+            $timeout(function() {
+              UserService.editProf(formData)
+                .then(
+                  function(res) {
+                    $scope.hideProg = true;
+                    console.log(res.data)
+                    UserService.profile();
+                   },
+                  function(res) {
+                  $scope.hideProg = true;
+                  $scope.msg = res.data.message;
+                });
+            $mdDialog.hide();
+            }, 3000);
         };
       }
+
+      $scope.deleteUser = function(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+        var confirm = $mdDialog.confirm()
+          .title('Would you like to delete your Account?')
+          .content('You will loose all your data if you confirm this. If you clicked this accidentally. Please, click CANCEL to exit')
+          .ariaLabel('Lucky day')
+          .ok('Please, Continue')
+          .cancel('CANCEL')
+          .targetEvent(ev);
+
+        $mdDialog.show(confirm).then(function() {
+          $rootScope.hideOutProg = false;
+          var def = UserService.deleteUser();
+          $timeout(function() {
+            def.then(function(res){
+              // console.log(res)
+              $window.sessionStorage.clear();
+              $window.location.href = '#/home';
+            });
+          }, 3000);
+        });
+      };
   }]);
