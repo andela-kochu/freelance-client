0a9d5fe2e8a275b9f33c64b8d33f2142053aacbf
andela-kochu <kingsley.ochu@andela.co>
2015-05-18 15:31:13 +0100

    Edited the jobservice to include the userJobs array

--- app/services/job.service.js
+++ app/services/job.service.js
@@ -4,18 +4,35 @@ angular.module('freelanceApp')
   .factory('JobService', ['$http', function($http) {
     var baseUrl = "http://freelance-app.herokuapp.com/api/v1";
     var jobObject = {
-      jobs: []
+      jobs: [],
+      userJobs: []
     };
+
     jobObject.getAll = function(){
       return $http.get(baseUrl + "/jobs").success(function(data){
         angular.copy(data, jobObject.jobs);
       });
     };
+
     jobObject.getSingle = function(params){
       return $http.get(baseUrl + "/jobs/" + params).then(function(res){
         return res.data;
       });
     };
+
+    jobObject.postJob = function(data){
+      return $http.post(baseUrl + "/jobs/", data).then(function(res){
+        return res.data;
+      });
+    };
+
+    jobObject.getUserJob = function(){
+      return $http.get(baseUrl + "/user/jobs/").then(function(res){
+        console.log(res.data)
+       angular.copy(res.data, jobObject.userJobs);
+      });
+    };
+
     return jobObject;
     /*jobObject.upvote = function(post){
       return $http.put('/posts/' + post._id + '/upvote').success(function(data){
